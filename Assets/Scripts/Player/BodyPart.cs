d5c508e55449dac96ea058884a4b2c0fdf63cee0
Xwilarg <xwilarg@protonmail.com>
2024-01-08 00:23:47 +0100

    Remove clothes on enemy collision

--- Assets/Scripts/Player/BodyPart.cs
+++ Assets/Scripts/Player/BodyPart.cs
@@ -6,11 +6,22 @@ namespace FlashSexJam.Player
     {
         public PlayerController Owner { set; private get; }
 
+        [SerializeField]
+        private GameObject _clothPart;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Enemy"))
             {
-                Debug.Log("hi");
+                if (_clothPart != null)
+                {
+                    Destroy(_clothPart);
+                    _clothPart = null;
+                }
+                else
+                {
+                    Debug.Log("ohno");
+                }
             }
         }
     }
