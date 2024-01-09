cd74a30cd18a57f3c82cedabcae9c9b07f22716a
Xwilarg <xwilarg@protonmail.com>
2024-01-09 13:52:52 +0100

    Add main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FlashSexJam.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
