381a42676b20e25344e99c14131d261f44e9fa46
Xwilarg <xwilarg@protonmail.com>
2024-01-09 15:33:28 +0100

    Add button to switch language

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using FlashSexJam.Translation;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace FlashSexJam.Menu
@@ -8,6 +9,14 @@ namespace FlashSexJam.Menu
         public void Play()
         {
             SceneManager.LoadScene("Main");
+        }
+
+        public void SetLanguage()
+        {
+            if (Translate.Instance.CurrentLanguage == "english")
+                Translate.Instance.CurrentLanguage = "french";
+            else
+                Translate.Instance.CurrentLanguage = "english";
         }
     }
 }
