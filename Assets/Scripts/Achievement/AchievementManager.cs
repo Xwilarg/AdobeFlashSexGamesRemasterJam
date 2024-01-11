0fb5d9e8b4a1db6002151c0d03c918363620f8b2
Xwilarg <xwilarg@protonmail.com>
2024-01-11 14:00:00 +0100

    Add achievements

--- Assets/Scripts/Achievement/AchievementManager.cs
+++ Assets/Scripts/Achievement/AchievementManager.cs
@@ -45,11 +45,21 @@ namespace FlashSexJam.Achievement
 
         public Dictionary<AchievementID, Achievement> Achievements { get; } = new()
         {
+            { AchievementID.Victory, new() { Name = "One step toward purity", Description = "Complete a level" } },
+            { AchievementID.VictoryNoHScene, new() { Name = "Stainless", Description = "Complete a level without triggering any lewd scene" } },
+            { AchievementID.VictoryNoClothDamage, new() { Name = "Top fashion", Description = "Complete a level without loosing any cloth" } },
+            { AchievementID.VictoryFullPower, new() { Name = "3% of my real power", Description = "Complete a level without using your power" } },
+            { AchievementID.VictoryPerfect, new() { Name = "Katabasis", Description = "Complete a level without triggering any lewd scene nor loosing cloth, and without using your power" } }
         };
     }
 
     public enum AchievementID
     {
+        Victory,
+        VictoryNoHScene,
+        VictoryNoClothDamage,
+        VictoryFullPower,
+        VictoryPerfect
     }
 
     public record Achievement
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -3,6 +3,8 @@ using Buttplug.Client;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using FlashSexJam.Achievement;
+using System.Collections;
 
 namespace FlashSexJam.Manager
 {
@@ -64,6 +66,24 @@ namespace FlashSexJam.Manager
             _progressPlayerBar.localScale = new(_progress / _info.DestinationDistance, 1f, 1f);
             _progressBossBar.localScale = new(_progressBoss / _info.DestinationDistance, 1f, 1f);
 
+            if (_progress >= _info.DestinationDistance)
+            {
+                DidGameEnd = true;
+
+                var hasClothes = false;
+                var hasPower = false;
+                var hasNoHScenes = false;
+
+                AchievementManager.Instance.Unlock(AchievementID.Victory);
+                if (hasNoHScenes) AchievementManager.Instance.Unlock(AchievementID.VictoryNoHScene);
+                if (hasClothes) AchievementManager.Instance.Unlock(AchievementID.VictoryNoClothDamage);
+                if (hasPower) AchievementManager.Instance.Unlock(AchievementID.VictoryFullPower);
+                if (hasNoHScenes && hasClothes && hasPower) AchievementManager.Instance.Unlock(AchievementID.VictoryPerfect);
+
+                StartCoroutine(WaitAndDisplayVictory());
+                return;
+            }
+
             if (_spawnTimer <= 0)
             {
                 Instantiate(_info.SpawnableEnemies[Random.Range(0, _info.SpawnableEnemies.Length)], _spawnPoint.position + (Vector3.up * Random.Range(-4f, 4f)), Quaternion.identity);
@@ -78,6 +98,12 @@ namespace FlashSexJam.Manager
                 var c = _gameOverBackground.color;
                 _gameOverBackground.color = new(c.r, c.g, c.b, Mathf.Clamp01(_gameOverTimer / _gameOverTimerRef));
             }
+        }
+
+        private IEnumerator WaitAndDisplayVictory()
+        {
+            yield return new WaitForSeconds(1f);
+            SceneManager.LoadScene("MainMenu"); // TODO: Proper Victory
         }
 
         public void TriggerGameOver()
