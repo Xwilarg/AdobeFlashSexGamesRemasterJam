3200dc4674b4abee2f9ea511fdfc879cb8c4d78e
Xwilarg <xwilarg@protonmail.com>
2024-01-12 04:07:20 +0100

    Fix lamps not being destroyed

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -37,16 +37,13 @@ namespace FlashSexJam.Enemy
         protected virtual void Update()
         {
             _rb.velocity = Vector3.left * (GameManager.Instance.Speed + _xSpeedOffset);
-        }
 
-        public abstract (float Min, float Max) SpawnRange { get; }
-
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.collider.CompareTag("Boss"))
+            if (transform.position.x < -20f)
             {
                 Destroy(gameObject);
             }
         }
+
+        public abstract (float Min, float Max) SpawnRange { get; }
     }
 }
