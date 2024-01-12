51b557290566e44ab4aca69649d04644b2ab78c4
Xwilarg <xwilarg@protonmail.com>
2024-01-12 12:22:22 +0100

    Add lamp animations

--- Assets/Scripts/Enemy/Impl/LampEnemy.cs
+++ Assets/Scripts/Enemy/Impl/LampEnemy.cs
@@ -1,7 +1,31 @@
-﻿namespace FlashSexJam.Enemy.Impl
+﻿using System.Collections;
+using UnityEngine;
+
+namespace FlashSexJam.Enemy.Impl
 {
     public class LampEnemy : EnemyController
     {
+        [SerializeField]
+        private Animator _lampAnim;
+
         public override (float Min, float Max) SpawnRange => (-4f, -4f);
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            StartCoroutine(DoPatern());
+        }
+
+        private IEnumerator DoPatern()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(2f);
+                _lampAnim.SetBool("IsHigh", true);
+                yield return new WaitForSeconds(2f);
+                _lampAnim.SetBool("IsHigh", false);
+            }
+        }
     }
 }
